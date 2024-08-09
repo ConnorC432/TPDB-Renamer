fc315f99621fca839fe8c6541643d8a20e103149
connor <connor@connor>
2024-08-09 21:31:43 +0100

    separated usage options

--- rename.sh
+++ rename.sh
@@ -6,37 +6,8 @@ script_dir="$(cd "$(dirname "$0")" && pwd)"
 
 source $script_dir/options.sh
 
-#Help
-Display_help()
-{
-	echo "TPDB to Plex renamer options"
-	echo "Usage: $0 [OPTIONS] [ARGUMENTS]"
-	echo
-	echo
-	echo "Options:"
-	echo
-	echo "[-h|--help]"
-	echo
-	echo
-	echo "[-t|-tv]	[TV Directory]		[Second TV Directory...]"
-	echo "		Specify the root TV directory(s) to rename"
-	echo "		Leave empty to rename files in current directory"
-	echo
-	echo "[-f|-film]	[Film Directory]	[Second Film Directory...]"
-	echo "		Specify the root Film directory(s) to rename"
-	echo "		Leave empty to rename files in current directory"
-	echo
-	echo "[-d|-default]"
-	echo "		Rename both TV and Film default directory(s)"
-	echo
-	echo "[-dt|-defaulttv]"
-	echo "		Rename TV default directory(s)"
-	echo
-	echo "[-df|-defaultfilm]"
-	echo "		Rename Film default directory(s)"
-	echo
-	echo
-}
+#Usage
+source $script_dir/Usage.sh
 
 #Execute rename scripts
 execute_rename()
