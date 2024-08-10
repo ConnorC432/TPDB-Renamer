bf006959990e4f9617c8458ec7de45303ea2300f
connor <connor@connor>
2024-08-10 21:44:35 +0100

    removed symbolic link from install script

--- Linux/install.sh
+++ Linux/install.sh
@@ -2,6 +2,5 @@
 # curl https://raw.githubusercontent.com/ConnorC432/TPDB-Renamer/main/Linux/install.sh | sudo bash
 mkdir /usr/share/tpdb-rename
 curl https://raw.githubusercontent.com/ConnorC432/TPDB-Renamer/main/tpdb-rename.py -o /usr/share/tpdb-rename/tpdb-rename.py
-curl https://raw.githubusercontent.com/ConnorC432/TPDB-Renamer/main/Linux/tpdb-rename -o /usr/share/tpdb-rename/tpdb-rename
+curl https://raw.githubusercontent.com/ConnorC432/TPDB-Renamer/main/Linux/tpdb-rename -o /usr/bin/tpdb-rename
 chmod -R 755 /usr/share/tpdb-rename
-ln -s /usr/share/tpdb-rename/tpdb-rename.py /usr/bin/tpdb-rename
